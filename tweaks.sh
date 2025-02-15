917273da032ef158e285666a23ca86a0e33ac69c
whitelex <59625181+whitelex@users.noreply.github.com>
2025-02-15 15:45:05 -0500

    Update tweaks.sh

--- tweaks.sh
+++ tweaks.sh
@@ -10,3 +10,10 @@ rm -f $motd/50-motd-news
 wget -P "$motd" https://github.com/whitelex/secure-server/raw/refs/heads/main/00-header || { echo "Failed to download new 00-header" >&2; exit 1; }
 chmod +x "$motd/00-header" || { echo "Failed to make 00-header executable" >&2; exit 1; }
 
+wget -P "$motd" https://raw.githubusercontent.com/yboetz/motd/refs/heads/master/40-services || { echo "Failed to download new 40-services" >&2; exit 1; }
+chmod +x "$motd/40-services" || { echo "Failed to make 40-services executable" >&2; exit 1; }
+
+wget -P "$motd" https://raw.githubusercontent.com/yboetz/motd/refs/heads/master/20-sysinfo || { echo "Failed to download new 20-sysinfo" >&2; exit 1; }
+chmod +x "$motd/20-sysinfo" || { echo "Failed to make 20-sysinfo executable" >&2; exit 1; }
+
+echo "Operation Complete! Logout and login again to see the results."
