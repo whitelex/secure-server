7342c4a14a2bc63b97609b26b48faccbec274f7e
whitelex <59625181+whitelex@users.noreply.github.com>
2025-02-15 18:10:15 -0500

    Update centos-tweaks.sh

--- centos-tweaks.sh
+++ centos-tweaks.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 #
 # Tweaks
+
+distro=$(cat /etc/*-release | (grep "DISTRIB_ID" || grep "NAME") | head -n 1 | cut -d "=" -f 2- | tr -d '"')
+
+if [ "$distro" = "CentOS Stream" ]; then
+  echo "$distro"
+else
+  echo "Your system is $distro. Try using another tweak file for your system."
+  exit 1
+fi
+
 motd="/etc/update-motd.d/"
 
 rm -f $motd/00-header
 rm -f $motd/10-help-text
 rm -f $motd/50-motd-news
 
-wget -P "$motd" https://github.com/whitelex/secure-server/raw/refs/heads/main/00-centos-header || { echo "Failed to download new 00-header" >&2; exit 1; }
-chmod +x "$motd/00-centos-header" || { echo "Failed to make 00-header executable" >&2; exit 1; }
+wget -P "$motd" https://github.com/whitelex/secure-server/raw/refs/heads/main/00-centos-header || { echo "Failed to download new 00-centos-header" >&2; exit 1; }
+chmod +x "$motd/00-centos-header" || { echo "Failed to make 00-centos-header executable" >&2; exit 1; }
 
 wget -P "$motd" https://raw.githubusercontent.com/yboetz/motd/refs/heads/master/40-services || { echo "Failed to download new 40-services" >&2; exit 1; }
 chmod +x "$motd/40-services" || { echo "Failed to make 40-services executable" >&2; exit 1; }
