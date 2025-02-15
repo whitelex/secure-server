41b29595f1515a624f4c4a1863c7cfe178088eec
whitelex <59625181+whitelex@users.noreply.github.com>
2025-02-15 15:27:04 -0500

    Update tweaks.sh

--- tweaks.sh
+++ tweaks.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
 # Tweaks
-rm -f /etc/update-motd.d/00-header
-rm -f /etc/update-motd.d/10-help-text
-rm -f /etc/update-motd.d/50-motd-news
+motd="/etc/update-motd.d/"
 
-wget 
+rm -f $motd/00-header
+rm -f $motd/10-help-text
+rm -f $motd/50-motd-news
+
+wget -P "$motd" https://github.com/whitelex/secure-server/raw/refs/heads/main/00-header || { echo "Failed to download new 00-header" >&2; exit 1; }
+chmod +x "$motd/00-header" || { echo "Failed to make 00-header executable" >&2; exit 1; }
+
