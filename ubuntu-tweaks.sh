97f7ece9d829adb3718ec6c2003dca428e8b1851
whitelex <59625181+whitelex@users.noreply.github.com>
2025-02-15 16:05:44 -0500

    Update ubuntu-tweaks.sh

--- ubuntu-tweaks.sh
+++ ubuntu-tweaks.sh
@@ -7,8 +7,8 @@ rm -f $motd/00-header
 rm -f $motd/10-help-text
 rm -f $motd/50-motd-news
 
-wget -P "$motd" https://github.com/whitelex/secure-server/raw/refs/heads/main/00-ubuntu-header || { echo "Failed to download new 00-header" >&2; exit 1; }
-chmod +x "$motd/00-header" || { echo "Failed to make 00-header executable" >&2; exit 1; }
+wget -P "$motd" https://github.com/whitelex/secure-server/raw/refs/heads/main/00-ubuntu-header || { echo "Failed to download new 00-ubuntu-header" >&2; exit 1; }
+chmod +x "$motd/00-ubuntu-header" || { echo "Failed to make 00-header executable" >&2; exit 1; }
 
 wget -P "$motd" https://raw.githubusercontent.com/yboetz/motd/refs/heads/master/40-services || { echo "Failed to download new 40-services" >&2; exit 1; }
 chmod +x "$motd/40-services" || { echo "Failed to make 40-services executable" >&2; exit 1; }
